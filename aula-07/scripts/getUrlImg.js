e929c5bab5af4c91bd97f1ada0362d7d7d164736
Jonatan Ivanhoe da Silva Rocha <jonatanivanhoe@gmail.com>
2022-08-17 16:34:25 -0300

    add mesa aula 07 - eventos

---
+++ aula-07/scripts/getUrlImg.js
@@ -0,0 +1,35 @@
+//criar elemento h1
+
+const h1 = document.createElement('h1')
+h1.innerHTML = 'Galeria de Imagens'
+document.body.appendChild(h1)
+
+//criar 3 inputs e bloquear apos preenchimento
+
+function criarInputs(){
+    for(let i = 0; i < 3; i++){
+        let input = document.createElement('INPUT')
+        input.setAttribute('type', 'text')
+        //input.setAttribute('value', 'Test')
+        document.body.appendChild(input)
+        input.placeholder = 'Digite aqui'
+        //input.disabled = true
+
+        input.addEventListener('change', evento)
+        function evento(){
+            if(document.querySelectorAll('input').values !== ''){
+                input.disabled = true
+            } else {
+                input.disabled = false
+            }
+        }
+    }
+}
+
+criarInputs()
+
+//add botao enviar
+
+const botao = document.createElement('button')
+botao.innerHTML = 'Enviar'
+document.body.appendChild(botao)
