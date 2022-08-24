252564986d45a72f2a7fe2cc039f05d5b47b189c
Jonatan Rocha <jonatanivanhoe@gmail.com>
2022-08-23 22:15:37 -0300

    fixed function normalize

--- aula-11/mesa-de-trabalho/scripts/script.js
+++ aula-11/mesa-de-trabalho/scripts/script.js
@@ -9,28 +9,40 @@ form.addEventListener('submit', function(event){
     // console.log(nome, sobrenome)
     
     //normalizando dados
+
+    function nomalizarDados(){
+        //todos os dados maiusculos
+        const nomeMaiusculo = nome.toUpperCase()
+        const sobrenomeMaiusculo = sobrenome.toUpperCase()
+        const nomeCompletoMaiusculo = nomeMaiusculo + ' ' + sobrenomeMaiusculo
+        alert(`Nome completo maiúsculo: ${nomeCompletoMaiusculo}`)
+        
+        //todos os dados minusculos
+        const nomeMinusculo = nome.toLowerCase()
+        const sobrenomeMinusculo = sobrenome.toLowerCase()
+        const nomeCompletoMinusculo = nomeMinusculo + ' ' + sobrenomeMinusculo
+        alert(`Nome completo minúsculo: ${nomeCompletoMinusculo}`)
+        
+        //retirando os espaços
+        
+        let nomeSemEspaco = nome.trim()
+        let sobrenomeSemEspaco = sobrenome.trim()
+        console.log(nomeSemEspaco, sobrenomeSemEspaco)
+        
+        //substituindo 'a' por '@' e 'e' por '3'
+        
+        function replacer(){
+            
+            let replace1 = /a/gi
+            let newNome1 = nome.replace(replace1, '@')
+            
+            let replace2 = /e/gi
+            let newNome2 = nome.replace(replace2, '3')
+            console.log(newNome1, newNome2)
     
-    //todos os dados maiusculos
-    const nomeMaiusculo = nome.toUpperCase()
-    const sobrenomeMaiusculo = sobrenome.toUpperCase()
-    console.log(nomeMaiusculo, sobrenomeMaiusculo)
+        }
+        replacer()
+    }
+    nomalizarDados()
     
-    //todos os dados minusculos
-    const nomeMinusculo = nome.toLowerCase()
-    const sobrenomeMinusculo = sobrenome.toLowerCase()
-    console.log(nomeMinusculo, sobrenomeMinusculo)
-    
-    //retirando os espaços
-    
-    let nomeSemEspaco = nome.trim()
-    let sobrenomeSemEspaco = sobrenome.trim()
-    console.log(nomeSemEspaco, sobrenomeSemEspaco)
-    
-    //substituindo 'a' por '@' e 'e' por '3'
-    let replace1 = /a/gi
-    let newNome1 = nome.replace(replace1, '@')
-    
-    let replace2 = /e/gi
-    let newNome2 = nome.replace(replace2, '3')
-    console.log(newNome1, newNome2)
 })
