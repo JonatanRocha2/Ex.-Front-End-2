4d4e708507f96387a5ca5949e2e8dc22f7a1d87c
Jonatan Rocha <jonatanivanhoe@gmail.com>
2022-08-23 21:07:51 -0300

    correcao da mesa

--- aula-11/mesa-de-trabalho/scripts/script.js
+++ aula-11/mesa-de-trabalho/scripts/script.js
@@ -2,34 +2,33 @@
 let form = document.querySelector('form')
 form.addEventListener('submit', function(event){
     event.preventDefault()
+    //pegar os valores dos inputs
+    const nome = document.getElementById('nome').value
+    const sobrenome = document.getElementById('sobrenome').value
+    
+    // console.log(nome, sobrenome)
+    
+    //normalizando dados
+    
+    //todos os dados maiusculos
+    const nomeMaiusculo = nome.toUpperCase()
+    const sobrenomeMaiusculo = sobrenome.toUpperCase()
+    console.log(nomeMaiusculo, sobrenomeMaiusculo)
+    
+    //todos os dados minusculos
+    const nomeMinusculo = nome.toLowerCase()
+    const sobrenomeMinusculo = sobrenome.toLowerCase()
+    console.log(nomeMinusculo, sobrenomeMinusculo)
+    
+    //retirando os espaços
+    
+    console.log(nome.trim(), sobrenome.trim())
+    
+    //substituindo 'a' por '@' e 'e' por '3'
+    let replace1 = /a/gi
+    let newNome1 = nome.replace(replace1, '@')
+    
+    let replace2 = /e/gi
+    let newNome2 = nome.replace(replace2, '3')
+    console.log(newNome1, newNome2)
 })
-
-//pegar os valores dos inputs
-const nome = document.getElementById('nome').value
-const sobrenome = document.getElementById('sobrenome').value
-
-// console.log(nome, sobrenome)
-
-//normalizando dados
-
-//todos os dados maiusculos
-const nomeMaiusculo = nome.toUpperCase()
-const sobrenomeMaiusculo = sobrenome.toUpperCase()
-console.log(nomeMaiusculo, sobrenomeMaiusculo)
-
-//todos os dados minusculos
-const nomeMinusculo = nome.toLowerCase()
-const sobrenomeMinusculo = sobrenome.toLowerCase()
-console.log(nomeMinusculo, sobrenomeMinusculo)
-
-//retirando os espaços
-
-
-
-//substituindo 'a' por '@' e 'e' por '3'
-let replace1 = /a/gi
-let newNome1 = nome.replace(replace1, '@')
-
-let replace2 = /e/gi
-let newNome2 = nome.replace(replace2, '3')
-console.log(newNome1, newNome2)
